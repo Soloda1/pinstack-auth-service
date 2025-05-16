2049364599f82c726f2a793aaf1ac32c2674d74c
Soloda1 <enderderder@gmail.com>
2025-05-16 19:31:14 +0400

    Add mocks

--- internal/auth/interface.go
+++ internal/auth/interface.go
@@ -1,5 +1,6 @@
 package auth
 
+//go:generate mockery --name TokenManager --dir . --output ../../mocks --outpkg mocks --with-expecter
 type TokenManager interface {
 	NewJWT(userID int64) (*TokenPair, error)
 	ParseRefreshToken(tokenString string) (*TokenClaims, error)
